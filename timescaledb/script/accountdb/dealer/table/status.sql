8beb2119fff05a6986ab8d5c24211cc62103c5a5
twhsu <twhsu@nlg.csie.ntu.edu.tw>
2023-05-12 11:11:36 +0800

    feat: update status

--- timescaledb/script/accountdb/dealer/table/status.sql
+++ timescaledb/script/accountdb/dealer/table/status.sql
@@ -3,9 +3,10 @@ CREATE TABLE accountdb.dealer.status(
     strategy INT,
     security_type char(1), -- S, F
     code varchar(10),
-    action char(1), -- B, S
+    cost double precision,
+    qty INT,
     in_date date,
     out_date date,
-    status varchar(10),
+    --status varchar(10),
     CONSTRAINT fk_strategy FOREIGN KEY(strategy) REFERENCES dealer.strategy(id)
 );
